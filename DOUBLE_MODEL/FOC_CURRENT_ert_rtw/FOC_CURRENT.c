cdeb8fb4ad3309cfa2ae4eea5095214af34fbb78
wanjietian <wanjie.tian@chaoli-electric.com>
2026-04-14 18:13:19 +0800

    foc current c code generate

--- DOUBLE_MODEL/FOC_CURRENT_ert_rtw/FOC_CURRENT.c
+++ DOUBLE_MODEL/FOC_CURRENT_ert_rtw/FOC_CURRENT.c
@@ -1,140 +1,52 @@
-/*
- * File: FOC_CURRENT.c
- *
- * Code generated for Simulink model 'FOC_CURRENT'.
- *
- * Model version                  : 8.146
- * Simulink Coder version         : 24.1 (R2024a) 19-Nov-2023
- * C/C++ source code generated on : Tue Apr 14 17:56:48 2026
- *
- * Target selection: ert.tlc
- * Embedded hardware selection: ARM Compatible->ARM Cortex-M
- * Code generation objectives:
- *    1. Execution efficiency
- *    2. RAM efficiency
- * Validation result: Not run
- */
-
 #include "FOC_CURRENT.h"
 #include "jscl_transform.h"
 #include "rtwtypes.h"
 #include <math.h>
 
-/* Block signals and states (default storage) */
-DW rtDW;
-
-/* External inputs (root inport signals with default storage) */
-ExtU rtU;
-
-/* External outputs (root outports fed by signals with default storage) */
-ExtY rtY;
-static void SVPWM1(void);
-
-/* Output and update for atomic system: '<S1>/SVPWM1' */
-static void SVPWM1(void)
+g_ty_DW g_l5;
+g_ty_ExtU g_m;
+g_ty_ExtY g_ho;
+static void t_p(void);
+static void t_p(void)
 {
-    VectorUVW_T rtb_spwm;
-    real_T rtb_Sum2_idx_0;
-    real_T rtb_Sum2_idx_1;
-    real_T rtb_eit;
-
-    /* CCaller: '<S2>/C Caller' */
-    InverseClarkeTransform(&rtDW.CCaller3, &rtb_spwm);
-
-    /* CCaller: '<S2>/C Caller1' */
-    CalcZeroSequenceVoltage(&rtb_spwm, &rtb_eit);
-
-    /* Sum: '<S2>/Sum2' */
-    rtb_Sum2_idx_0 = rtb_eit + rtb_spwm.s64_u;
-    rtb_Sum2_idx_1 = rtb_eit + rtb_spwm.s64_v;
-    rtb_eit += rtb_spwm.s64_w;
-
-    /* Gain: '<S2>/Gain3' incorporates:
-     *  Constant: '<S2>/Constant2'
-     *  Gain: '<S2>/Gain2'
-     *  Inport: '<Root>/Vbus'
-     *  Product: '<S2>/Divide1'
-     *  Sum: '<S2>/Sum3'
-     */
-    rtDW.Gain3[0] = (-rtb_Sum2_idx_0 / rtU.Vbus + 0.5) * 4199.0;
-    rtDW.Gain3[1] = (-rtb_Sum2_idx_1 / rtU.Vbus + 0.5) * 4199.0;
-    rtDW.Gain3[2] = (-rtb_eit / rtU.Vbus + 0.5) * 4199.0;
+    VectorUVW_T local_;
+    real_T local_bw;
+    real_T local_gb_idx_0;
+    real_T local_gb_idx_1;
+    InverseClarkeTransform(&g_l5.g_f_p, &local_);
+    CalcZeroSequenceVoltage(&local_, &local_bw);
+    local_gb_idx_0 = local_bw + local_.s64_u;
+    local_gb_idx_1 = local_bw + local_.s64_v;
+    local_bw += local_.s64_w;
+    g_l5.g_f_b[0] = (-local_gb_idx_0 / g_m.g_f_m + 0.5) * 4199.0;
+    g_l5.g_f_b[1] = (-local_gb_idx_1 / g_m.g_f_m + 0.5) * 4199.0;
+    g_l5.g_f_b[2] = (-local_bw / g_m.g_f_m + 0.5) * 4199.0;
 }
 
-/* Model step function */
 void FOC_CURRENT_step(void)
 {
-    VectorAB_T rtb_ClarkeTransform;
-    VectorDQ_T rtb_CCaller2;
-    VectorDQ_T rtb_ParkTransform;
-    VectorUVW_T rtb_BusConversion_InsertedFor_C;
-    real_T rtb_TmpSignalConversionAtCCalle[2];
-
-    /* Outputs for Atomic SubSystem: '<Root>/FOC_CURRENT' */
-    /* SignalConversion generated from: '<S1>/C Caller3' incorporates:
-     *  Inport: '<Root>/Theta'
-     *  Trigonometry: '<S3>/SinCos'
-     */
-    rtb_TmpSignalConversionAtCCalle[0] = sin(rtU.theta);
-    rtb_TmpSignalConversionAtCCalle[1] = cos(rtU.theta);
-
-    /* BusCreator generated from: '<S1>/ClarkeTransform' incorporates:
-     *  Inport: '<Root>/s64_u'
-     *  Inport: '<Root>/s64_v'
-     *  Inport: '<Root>/s64_w'
-     */
-    rtb_BusConversion_InsertedFor_C.s64_u = rtU.s64_u;
-    rtb_BusConversion_InsertedFor_C.s64_v = rtU.s64_v;
-    rtb_BusConversion_InsertedFor_C.s64_w = rtU.s64_w;
-
-    /* CCaller: '<S1>/ClarkeTransform' */
-    ClarkeTransform(&rtb_BusConversion_InsertedFor_C, &rtb_ClarkeTransform);
-
-    /* CCaller: '<S1>/ParkTransform' */
-    ParkTransform(&rtb_ClarkeTransform, &rtb_TmpSignalConversionAtCCalle[0],
-                  &rtb_ParkTransform);
-
-    /* CCaller: '<S1>/C Caller2' incorporates:
-     *  Constant: '<S1>/d_ki'
-     *  Constant: '<S1>/d_kp'
-     *  Constant: '<S1>/q_ki'
-     *  Constant: '<S1>/q_kp'
-     *  Inport: '<Root>/IRefD'
-     *  Inport: '<Root>/IRefQ'
-     */
-    CurrentLoopDQ(rtU.IRefD, rtb_ParkTransform.s64_d, rtU.Signal1,
-                  rtb_ParkTransform.s64_q, 0.24527670483636954,
-                  221.54511393115223, 0.24527670483636954, 221.54511393115223,
-                  &rtb_CCaller2);
-
-    /* CCaller: '<S1>/C Caller3' */
-    InverseParkTransform(&rtb_CCaller2, &rtb_TmpSignalConversionAtCCalle[0],
-                         &rtDW.CCaller3);
-
-    /* Outputs for Atomic SubSystem: '<S1>/SVPWM1' */
-    SVPWM1();
-
-    /* End of Outputs for SubSystem: '<S1>/SVPWM1' */
-    /* End of Outputs for SubSystem: '<Root>/FOC_CURRENT' */
-
-    /* Outport: '<Root>/tAout' */
-    rtY.tAout = rtDW.Gain3[0];
-
-    /* Outport: '<Root>/tBout' */
-    rtY.tBout = rtDW.Gain3[1];
-
-    /* Outport: '<Root>/tCout' */
-    rtY.tCout = rtDW.Gain3[2];
+    VectorAB_T local_gy;
+    VectorDQ_T local_b;
+    VectorDQ_T local_e;
+    VectorUVW_T local_l;
+    real_T local_[2];
+    local_[0] = sin(g_m.g_f_e);
+    local_[1] = cos(g_m.g_f_e);
+    local_l.s64_u = g_m.g_f_a;
+    local_l.s64_v = g_m.g_f_o;
+    local_l.s64_w = g_m.g_f_hk;
+    ClarkeTransform(&local_l, &local_gy);
+    ParkTransform(&local_gy, &local_[0], &local_e);
+    CurrentLoopDQ(g_m.g_f_j, local_e.s64_d, g_m.g_f_h, local_e.s64_q,
+                  0.24527670483636954, 221.54511393115223, 0.24527670483636954,
+                  221.54511393115223, &local_b);
+    InverseParkTransform(&local_b, &local_[0], &g_l5.g_f_p);
+    t_p();
+    g_ho.g_f_f = g_l5.g_f_b[0];
+    g_ho.g_f_i = g_l5.g_f_b[1];
+    g_ho.g_f_h = g_l5.g_f_b[2];
 }
 
-/* Model initialize function */
 void FOC_CURRENT_initialize(void)
 {
-    /* (no initialization code required) */
 }
-
-/*
- * File trailer for generated code.
- *
- * [EOF]
- */
--- DOUBLE_MODEL/FOC_CURRENT_ert_rtw/FOC_CURRENT.h
+++ DOUBLE_MODEL/FOC_CURRENT_ert_rtw/FOC_CURRENT.h
@@ -1,105 +1,41 @@
-/*
- * File: FOC_CURRENT.h
- *
- * Code generated for Simulink model 'FOC_CURRENT'.
- *
- * Model version                  : 8.146
- * Simulink Coder version         : 24.1 (R2024a) 19-Nov-2023
- * C/C++ source code generated on : Tue Apr 14 17:56:48 2026
- *
- * Target selection: ert.tlc
- * Embedded hardware selection: ARM Compatible->ARM Cortex-M
- * Code generation objectives:
- *    1. Execution efficiency
- *    2. RAM efficiency
- * Validation result: Not run
- */
-
 #ifndef FOC_CURRENT_h_
 #define FOC_CURRENT_h_
 #ifndef FOC_CURRENT_COMMON_INCLUDES_
 #define FOC_CURRENT_COMMON_INCLUDES_
 #include "rtwtypes.h"
-#endif                                 /* FOC_CURRENT_COMMON_INCLUDES_ */
+#endif
 
 #include "jscl_transform.h"
 
-/* user code (top of header file) */
 #include "jscl_clarke.h"
 #include "jscl_park.h"
 
-/* Block signals and states (default storage) for system '<Root>' */
 typedef struct {
-    VectorAB_T CCaller3;               /* '<S1>/C Caller3' */
-    real_T Gain3[3];                   /* '<S2>/Gain3' */
-} DW;
+    VectorAB_T g_f_p;
+    real_T g_f_b[3];
+} g_ty_DW;
 
-/* External inputs (root inport signals with default storage) */
 typedef struct {
-    real_T theta;                      /* '<Root>/Theta' */
-    real_T Vbus;                       /* '<Root>/Vbus' */
-    real_T IRefD;                      /* '<Root>/IRefD' */
-    real_T Signal1;                    /* '<Root>/IRefQ' */
-    real_T s64_u;                      /* '<Root>/s64_u' */
-    real_T s64_v;                      /* '<Root>/s64_v' */
-    real_T s64_w;                      /* '<Root>/s64_w' */
-} ExtU;
+    real_T g_f_e;
+    real_T g_f_m;
+    real_T g_f_j;
+    real_T g_f_h;
+    real_T g_f_a;
+    real_T g_f_o;
+    real_T g_f_hk;
+} g_ty_ExtU;
 
-/* External outputs (root outports fed by signals with default storage) */
 typedef struct {
-    real_T tAout;                      /* '<Root>/tAout' */
-    real_T tBout;                      /* '<Root>/tBout' */
-    real_T tCout;                      /* '<Root>/tCout' */
-} ExtY;
+    real_T g_f_f;
+    real_T g_f_i;
+    real_T g_f_h;
+} g_ty_ExtY;
 
-/* Block signals and states (default storage) */
-extern DW rtDW;
-
-/* External inputs (root inport signals with default storage) */
-extern ExtU rtU;
-
-/* External outputs (root outports fed by signals with default storage) */
-extern ExtY rtY;
-
-/* Model entry point functions */
+extern g_ty_DW g_l5;
+extern g_ty_ExtU g_m;
+extern g_ty_ExtY g_ho;
 extern void FOC_CURRENT_initialize(void);
 extern void FOC_CURRENT_step(void);
 
-/*-
- * These blocks were eliminated from the model due to optimizations:
- *
- * Block '<S1>/Rate Transition1' : Eliminated since input and output rates are identical
- * Block '<S1>/Rate Transition2' : Eliminated since input and output rates are identical
- * Block '<S1>/Rate Transition3' : Eliminated since input and output rates are identical
- * Block '<S1>/Rate Transition8' : Eliminated since input and output rates are identical
- */
+#endif
 
-/*-
- * The generated code includes comments that allow you to trace directly
- * back to the appropriate location in the model.  The basic format
- * is <system>/block_name, where system is the system number (uniquely
- * assigned by Simulink) and block_name is the name of the block.
- *
- * Note that this particular code originates from a subsystem build,
- * and has its own system numbers different from the parent model.
- * Refer to the system hierarchy for this subsystem below, and use the
- * MATLAB hilite_system command to trace the generated code back
- * to the parent model.  For example,
- *
- * hilite_system('FOC_CURRENTLOOP_DOUBLE/FOC/FOC_CURRENT')    - opens subsystem FOC_CURRENTLOOP_DOUBLE/FOC/FOC_CURRENT
- * hilite_system('FOC_CURRENTLOOP_DOUBLE/FOC/FOC_CURRENT/Kp') - opens and selects block Kp
- *
- * Here is the system hierarchy for this model
- *
- * '<Root>' : 'FOC_CURRENTLOOP_DOUBLE/FOC'
- * '<S1>'   : 'FOC_CURRENTLOOP_DOUBLE/FOC/FOC_CURRENT'
- * '<S2>'   : 'FOC_CURRENTLOOP_DOUBLE/FOC/FOC_CURRENT/SVPWM1'
- * '<S3>'   : 'FOC_CURRENTLOOP_DOUBLE/FOC/FOC_CURRENT/sincos'
- */
-#endif                                 /* FOC_CURRENT_h_ */
-
-/*
- * File trailer for generated code.
- *
- * [EOF]
- */
